1894fa704f4a7eb9011c980581b2e3554c48394c
sebuahsemangat <mrsyntax25@gmail.com>
2023-05-23 14:36:11 +0700

    Adding form to index.php

--- index.php
+++ index.php
@@ -4,9 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Similarity Checker</title>
 </head>
 <body>
-    
+    <form action="check.php" method="post">
+        Text 1: <textarea name="text1" id="" cols="30" rows="10"></textarea>
+        <hr>
+        Text 2: <textarea name="text2" id="" cols="30" rows="10"></textarea>
+        <hr>
+        <button type="submit">Check Similarity</button>
+    </form>
 </body>
 </html>
